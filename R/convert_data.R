be9cb911e2ebae924ed71d626a7e1b68ad78a8a4
Thiseas Christos Lamnidis <thisseass@gmail.com>
2022-05-02 18:25:12 +0200

    Add ras to rasta conversion and jackknifing functions

---
+++ R/convert_data.R
@@ -0,0 +1,77 @@
+if (getRversion() >= "2.15.1") utils::globalVariables(c(".", "Norm","rasta")) ## Disables notes about '.' due to magrittr
+
+#' A function to calculate RASTA(A, B; C, Ref) from a blockTable of RAS per chromosome.
+#'
+#' @param block_table tibble. A tibble with the contents of a blockTableFile, derived with read_xerxes_ras_blocks()
+#' @param A character. The Lefts to cycle as population A.
+#' @param B character. The Lefts to cycle as population B.
+#' @param C character. The Rights to cycle as population C.
+#'
+#' @return A tibble with the all the requested combinations of rasta, along with their SE and Zscores
+#' @export
+#'
+#' @importFrom magrittr %>%
+#' @importFrom rlang .data
+#'
+ras_to_rasta <- function(block_table, A, B, C) {
+
+  ## Check that all populations were provided, and requested populations exist in the input
+  check_x_in_y(A, block_table$Left, "A")
+  check_x_in_y(B, block_table$Left, "B")
+  check_x_in_y(C, block_table$Right, "C")
+
+  ## Convert ras to raw ras
+  raw_ras <- block_table %>%
+    dplyr::mutate(RAS=.data$RAS*.data$Norm) %>%
+    dplyr::select(.data$Left, .data$Right, .data$BlockNr, .data$RAS, .data$Norm)
+    tidyr::pivot_wider(names_from=.data$Right, values_from=.data$RAS)
+
+  tidyr::crossing(A,B,C) %>% purrr::pmap_dfr(., ~calculate_rasta(raw_ras, A=..1, B=..2, C=..3))
+}
+
+
+#' Calculate a specific RASTA(A, B, C, Ref) from an input block table.
+#'
+#' @param raw_ras tibble. A tibble containing the results
+#' @param A character. Population A fr the rasta calculation. Must be present in the raw_ras$Left.
+#' @param B character. Population B for the rasta calculation. Must be present in the raw_ras$Left.
+#' @param C character. Population C for the rasta calculation. Must be present in the raw_ras$Right.
+#'
+#' @return A tibble with the requested RASTA, it's error and ZScore
+#' @export
+calculate_rasta <- function(raw_ras, A, B, C) {
+
+  ras_A_C <- raw_ras %>%
+    dplyr::filter(.data$Left == A, .data$Right == C)
+
+  ras_B_C <- raw_ras %>%
+    dplyr::filter(.data$Left == B, .data$Right == C)
+
+  rasta_table <- dplyr::full_join(ras_A_C, ras_B_C, by=c("Right", "BlockNr","Norm")) %>%
+    dplyr::rename(A=.data$Left.x, B=.data$Left.y, C=.data$Right) %>%
+    dplyr::mutate(rasta=.data$RAS.x - .data$RAS.y, D="Ref") %>%
+    dplyr::select(.data$A, .data$B, .data$C, .data$D, .data$BlockNr, .data$Norm, .data$rasta)
+
+  result <- delete_mj_jackknife(rasta_table, rasta, Norm, Norm) %>% tibble::as.tibble() %>%
+    dplyr::mutate(A=A, B=B, C=C, D="Ref") %>%
+    dplyr::select(.data$A, .data$B, .data$C, .data$D, .data$theta_J, .data$jack_se, .data$Zscore)
+
+  return(result)
+}
+
+#' Check that x is not empty and all elements exist in y.
+#'
+#' @param x character. A vector of strings that must exist in y.
+#' @param y character. A vector of character that must include all elements in x.
+#' @param var_name character. The variable name to be included in error messages.
+#'
+#' @export
+#'
+check_x_in_y <- function(x, y, var_name) {
+  assertthat::assert_that(length(x) != 0, msg = paste0("[ras_to_rasta]: '", var_name, "' cannot be empty."))
+
+  assertthat::assert_that(
+    setequal(x, intersect(x, y))
+    , msg = paste0("[ras_to_rasta]: ID(s) '", paste0(setdiff(x, y), collapse="', "), "' not found in input table. Halting execution.")
+  )
+}
---
+++ R/stats.R
@@ -0,0 +1,66 @@
+#' Delete mj jackknife function
+#'
+#' Implementation of delete mj jackknifing following Nick Patterson's writeup of it.
+#' The function works like tidyverse functions, i.e. values, mj and weights should not be quoted/be symbols.
+#'
+#' @param data Tibble containing the data to use.
+#' @param values Unquoted name of the tibble column with the block values.
+#' @param mj Unquoted name of the tibble column with the mj values.
+#' @param weights Unquoted name of the tibble column with the block weights.
+#' @param .print_vals logical. Should the actual theta_minus values be printed?
+#'
+#' @return list. Returns a list of items :
+#' theta_J  The jackknife mean from all the theta_-x data.
+#' jack_se The SE of theta_J
+#' Zscore  The Zscore of the theta_J
+#' jack.values A vector of all the theta_-x values
+#' @export
+#'
+delete_mj_jackknife <- function(data, values, mj, weights, .print_vals=F) {
+  values <- rlang::enquo(values)
+  mj <- rlang::enquo(mj)
+  weights <- rlang::enquo(weights)
+
+  data <- data %>%  dplyr::ungroup() ## Remove grouping from the data to avoid summarise mishaps
+
+  ## Calculate the sum of each column as well as theta_hat
+  sums <- data %>%  dplyr::summarise(value_sum=sum(!!values),     ## The sum of values
+                                     mj_sum=sum(!!mj),            ## The sum of mj. used for calculating rates of the values
+                                     weight_sum=sum(!!weights),   ## The sum of weights across all blocks
+                                     theta_hat=.data$value_sum/.data$mj_sum   ## Theta hat as a rate of values across all blocks
+  )
+
+  ## Add columns with the theta_minus, as well as n_minus_weight
+  data <- data %>% dplyr::mutate(
+    value_minus=sums$value_sum-!!values,        ## value total - value of block
+    mj_minus=sums$mj_sum-!!mj,                  ## mj total - mj of block
+    n_minus_weight=sums$weight_sum-!!weights,   ## weight total - weight of block
+    theta_minus=.data$value_minus/.data$mj_minus,           ## The rate calculated from all but the given block.
+    hj=sums$weight_sum/!!weights,               ## hj per block for tau calculation
+    tau=.data$hj*sums$theta_hat - (.data$hj-1)* .data$theta_minus ## pseudovalue for variance calculation
+  )
+  ## Use added columns to calculate theta_J
+  theta_J <- data %>%
+    dplyr::summarise(theta_J=nrow(data) * sums$theta_hat - sum(.data$n_minus_weight*.data$theta_minus/sums$weight_sum)) %>%
+    dplyr::pull(.data$theta_J)
+  variance <- data %>%  dplyr::summarise(variance=1/nrow(data) * sum((.data$tau-.data$theta_J)^2/(.data$hj-1))) %>% dplyr::pull(.data$variance)
+  jack_se = sqrt(.data$variance)
+  if (.print_vals) {
+    return(
+      list(
+        theta_J=theta_J,
+        jack_se=jack_se,
+        Zscore=theta_J/jack_se,
+        jack.values=as.vector(data$theta_minus)
+      )
+    )
+  } else {
+    return(
+      list(
+        theta_J=theta_J,
+        jack_se=jack_se,
+        Zscore=theta_J/jack_se
+      )
+    )
+  }
+}
