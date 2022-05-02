d3ea8f1b7a018190cde85499735cab62e7aa1db1
Thiseas Christos Lamnidis <thisseass@gmail.com>
2022-05-02 12:23:08 +0200

    Typo

--- R/import_data.R
+++ R/import_data.R
@@ -1,7 +1,7 @@
 #' read_xerxes_ras_blocks
 #'
 #' @param fn path. The path to the input blockTableFile.
-#' @param stip_selection_language logical. Should trident selection language marks be stripped from Lefts and Rights?
+#' @param strip_selection_language logical. Should trident selection language marks be stripped from Lefts and Rights?
 #' If true, selection marks such as "<" and ">" will be stripped from the Left and Right columns.
 #'
 #' @return A tibble containing the contents of the blockTableFile
@@ -9,7 +9,7 @@
 #'
 #' @importFrom magrittr %>%
 #' @importFrom rlang .data
-read_xerxes_ras_blocks <- function(fn, stip_selection_language = F) {
+read_xerxes_ras_blocks <- function(fn, strip_selection_language = F) {
   if (!file.exists(fn)) {
     stop(paste0(
       "[read_xerxes_ras_blocks] File '",fn, "' not found. Please provide the path to a valid xerxes ras blockTableFile."
@@ -18,7 +18,7 @@ read_xerxes_ras_blocks <- function(fn, stip_selection_language = F) {
 
   block_table <- readr::read_tsv(fn, col_types = 'ccicicidd')
 
-  if (stip_selection_language) {
+  if (strip_selection_language) {
     block_table <- block_table %>%
       dplyr::mutate(dplyr::across(
         .cols=c(.data$Left, .data$Right),
